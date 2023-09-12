284daf88529bccdd31940d7c11e5c28314c1db7e
PRAGYANSHREE DAS <pragyan9423@gmail.com>
2023-09-12 20:03:06 +0530

    instrumentation test writing

--- app/src/androidTest/java/com/ocean/tipcaladv/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ocean/tipcaladv/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.ocean.tipcaladv
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +23,21 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_20_percent_tip(){
+        Espresso.onView(ViewMatchers.withId(R.id.cost_of_service_edit_text))
+            .perform(ViewActions.typeText("50.00"))
+
+        Espresso.onView(ViewMatchers.withId(R.id.calculate_button)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.tip_result))
+            .check(ViewAssertions.matches(ViewMatchers.withText(CoreMatchers.containsString("$10.00"))))
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
